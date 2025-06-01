4851f58795503f34260ef6f7db82ba7930574ef6
Ahad12345-code <nad121318@gmail.com>
2025-06-01 02:18:56 -0700

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,10 @@
+name="penguin"
+age=15
+weight=45.9
+student=True
+print("type of name",type (name))
+print("type of age",type(age))
+print("type of weight",type(weight))
+print("type of student",type(student))
+age=str(age)
+print("type of age",type(age))
