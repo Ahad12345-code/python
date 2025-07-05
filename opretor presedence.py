f6988650eaf5e4e01e89947110a18de71b0b8281
Ahad12345-code <nad121318@gmail.com>
2025-07-05 08:32:08 -0700

    Add files via upload

---
+++ opretor presedence.py
@@ -0,0 +1,13 @@
+x=10
+z=5
+s=6
+v=9
+y=0
+y= (x+v)*s/z;
+print("value of (x+v) * s/z is ",z)
+name= "alex"
+age= 0
+if name == "alex" or name== "jhon" and age >=2:
+    print("hello!welcome")
+else:
+    print("goodbye")
