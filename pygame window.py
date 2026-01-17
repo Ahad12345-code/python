7a1d28fc87a93f69b95073207c838d758cd0c485
Ahad12345-code <nad121318@gmail.com>
2026-01-16 23:58:31 -0800

    Add files via upload

---
+++ pygame window.py
@@ -0,0 +1,12 @@
+
+from tkinter import *
+
+# Create Window
+window = Tk()
+
+# Set the window Title and Geometry
+window.title('Demo Window')
+window.geometry('400x300')
+
+# Start the GUI vent loop
+window.mainloop()
